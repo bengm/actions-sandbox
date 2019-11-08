e2480f458f828e207c5ebdf2c8f45eb5b749b154
Ben Morris <benmorris@Bens-MacBook-Pro.local>
2019-11-08 14:52:48 -0500

    smell

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     let foo = 5;
+    let bar = 6;
+    let fiz = 0;
+    fiz = foo + bar;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {fiz}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
